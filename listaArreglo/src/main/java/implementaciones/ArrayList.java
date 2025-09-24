fd0bbb58d9ad9fc6c6c098d6b38e80c90ced4e59
luisc <luisc@LuisCa>
2025-09-23 22:50:15 -0700

    Prueba GGs

--- listaArreglo/src/main/java/implementaciones/ArrayList.java
+++ listaArreglo/src/main/java/implementaciones/ArrayList.java
@@ -13,6 +13,7 @@ public class ArrayList<T> implements IList<T>, Iterable<T>{
     @Override
     public void set(T o, int i) throws ListException {
         // Hola que hace GG
+        // Hola que hace GG
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
